727d012210b432741d54f2cd07bfd4f3b8d1adce
vishal <vishal@qburst.com>
2015-09-21 11:53:10 +0530

    responsive

--- shopping cart/js/script.js
+++ shopping cart/js/script.js
@@ -26,15 +26,15 @@ app.controller("cartController", ["$scope", function ($scope) {
     $scope.productCount;
     $scope.status = "";
     $scope.prods = [
-        {name : "Shirts", price : 100, availability : 10, path : "../shopping cart/assets/0.jpg", count : 0},
-        {name : "Accessories", price : 50, availability : 7, path : "../shopping cart/assets/1.jpg", count : 0},
-        {name : "Sports", price : 200, availability : 21, path : "../shopping cart/assets/2.jpg", count : 0},
-        {name : "Mobiles", price : 1000, availability : 14, path : "../shopping cart/assets/3.jpg", count : 0},
-        {name : "Books", price : 200, availability : 16, path : "../shopping cart/assets/4.jpg", count : 0},
-        {name : "Computer", price : 2000, availability : 11, path : "../shopping cart/assets/5.jpg", count : 0},
-        {name : "Furniture", price : 1500, availability : 5, path : "../shopping cart/assets/6.jpg", count : 0},
-        {name : "Shoes", price : 150, availability : 8, path : "../shopping cart/assets/7.jpg", count : 0},
-        {name : "Bags", price : 150, availability : 6, path : "../shopping cart/assets/8.jpg", count : 0}
+        {name : "Shirts", price : 100, availability : 10, path : "assets/0.jpg", count : 0},
+        {name : "Accessories", price : 50, availability : 7, path : "assets/1.jpg", count : 0},
+        {name : "Sports", price : 200, availability : 21, path : "assets/2.jpg", count : 0},
+        {name : "Mobiles", price : 1000, availability : 14, path : "assets/3.jpg", count : 0},
+        {name : "Books", price : 200, availability : 16, path : "assets/4.jpg", count : 0},
+        {name : "Computer", price : 2000, availability : 11, path : "assets/5.jpg", count : 0},
+        {name : "Furniture", price : 1500, availability : 5, path : "assets/6.jpg", count : 0},
+        {name : "Shoes", price : 150, availability : 8, path : "assets/7.jpg", count : 0},
+        {name : "Bags", price : 150, availability : 6, path : "assets/8.jpg", count : 0}
     ];
     
     $scope.removeCart = function () {
